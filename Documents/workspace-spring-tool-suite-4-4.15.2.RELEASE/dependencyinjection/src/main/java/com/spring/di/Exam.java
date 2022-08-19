7992da408c299f8755b19dcbd1bfedca7eb94212
CNshilpa <shilpa.cnv@gmail.com>
2022-08-20 01:12:36 +0530

    Dependency Injection

---
+++ Documents/workspace-spring-tool-suite-4-4.15.2.RELEASE/dependencyinjection/src/main/java/com/spring/di/Exam.java
@@ -0,0 +1,15 @@
+package com.spring.di;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Exam {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+		Student std = context.getBean("student",Student.class);
+		std.displayStudentInfo();
+		
+	}
+}
